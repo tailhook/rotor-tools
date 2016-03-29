00f6c70f99c54e0723190c332a90d769d3e8c61c
Paul Colomiets <paul@colomiets.name>
2016-03-30 02:46:34 +0300

    New concept for Future object

--- src/future.rs
+++ src/future.rs
@@ -1,38 +1,52 @@
 use std::sync::{Arc, Mutex};
+use std::marker::PhantomData;
 
 use rotor::{Notifier, GenericScope};
 
-
-pub struct Future<T>(Arc<Mutex<Option<T>>>);
-
-pub struct Port<A: ?Sized, B: 'static>(Arc<Mutex<Option<B>>>, Notifier, fn(&A) -> B);
-
-pub fn pair<S: GenericScope, A: ?Sized, B: 'static>(scope: &mut S, fun: fn(&A) -> B)
-    -> (Future<B>, Port<A, B>)
-{
-    let arc = Arc::new(Mutex::new(None));
-    (Future(arc.clone()), Port(arc, scope.notifier(), fun))
+trait ReadFuture<T> {
+    fn ready(&self) -> bool;
+    fn take(&mut self) -> Option<T>;
 }
 
-impl<A: ?Sized, B: 'static> Port<A, B> {
-    /// Put the value into corresponding future
-    ///
-    /// Returns true if value is put (otherwise future is probably
-    /// already destoryed)
-    pub fn put(mut self, value: &A) -> bool {
-        let ref fun = self.2;
-        let real_value = fun(value);
-        let ok = self.0.lock().as_mut().map(|x| {
-            **x = Some(real_value)
-        }).is_ok();
-        // TODO(tailhook) this should be strong_count() but that is unstable
-        self.1.wakeup().is_ok() && ok && Arc::get_mut(&mut self.0).is_none()
+pub struct Future<T>(Arc<Mutex<ReadFuture<T>>>);
+
+pub struct FutureImpl<I, O, F: FnOnce(I) -> O>{
+    output: Option<O>,
+    convert: Option<F>,
+    notifier: Notifier,
+    phantom: PhantomData<*const I>,
+}
+
+impl<I, O, F:FnOnce(I) -> O> FutureImpl<I, O, F> {
+    pub fn new(fun: F, notify: Notifier) -> FutureImpl<I, O, F> {
+        FutureImpl {
+            output: None,
+            convert: Some(fun),
+            notifier: notify,
+            phantom: PhantomData,
+        }
+    }
+    pub fn put(&mut self, t: O) {
+        self.output = Some(t);
+        self.notifier.wakeup().expect("wakeup of state machine");
+    }
+    pub fn convert(&mut self) -> F {
+        self.convert.take().unwrap()
+    }
+}
+
+impl<I, O, F:FnOnce(I) -> O> ReadFuture<O> for FutureImpl<I, O, F> {
+    fn ready(&self) -> bool {
+        self.output.is_some()
+    }
+    fn take(&mut self) -> Option<O> {
+        self.output.take()
     }
 }
 
 impl<T: Sized> Future<T> {
     pub fn is_done(&self) -> bool {
-        self.0.lock().expect("future can be locked").is_some()
+        self.0.lock().expect("future can be locked").ready()
     }
     pub fn consume(self) -> Result<T, Self> {
         match self.0.lock().expect("future can be locked").take() {
@@ -43,21 +57,47 @@ impl<T: Sized> Future<T> {
     }
 }
 
+pub fn new<I, O: 'static, F:FnOnce(I) -> O + 'static, S: GenericScope>(scope: &mut S, fun: F)
+    -> Arc<Mutex<FutureImpl<I, O, F>>>
+{
+    Arc::new(Mutex::new(FutureImpl::new(fun, scope.notifier())))
+}
+
 #[cfg(test)]
 mod test {
     extern crate rotor_test;
+    use std::sync::{Arc, Mutex};
 
     use std::str::FromStr;
-    use super::{pair};
+    use super::{new, FutureImpl, Future};
+
+    trait ParseStr {
+        fn put_str(&mut self, &str);
+    }
+    struct Port(Arc<Mutex<ParseStr>>);
+
+    impl<'a, O, F: FnOnce(&str) -> O> ParseStr for FutureImpl<&'a str, O, F> {
+        fn put_str(&mut self, val: &str) {
+            let converted = self.convert()(val);
+            self.put(converted);
+        }
+    }
+
+    impl Port {
+        fn put(self, val: &str) {
+            self.0.lock().expect("port locked").put_str(val)
+        }
+    }
 
     #[test]
     fn test_int() {
         let mut lp = rotor_test::MockLoop::new(());
         let ref mut scope = lp.scope(1);
-        let (future, port) = pair(scope, u32::from_str);
+        let arc = new(scope, |x: &str| x.parse().unwrap());
+        let future: Future<u64> = Future(arc.clone());
         let value: String = String::from("10");
-        port.put(&value[..]);
-        assert_eq!(future.consume().unwrap_or(Ok(9999)), Ok(10));
+        Port(arc).put(&value[..]);
+        assert_eq!(future.consume().unwrap_or(9999), 10);
     }
 
 }
