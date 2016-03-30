a022a2304af5ea526793178ee4cab15c10a33afa
Paul Colomiets <paul@colomiets.name>
2016-03-31 02:11:00 +0300

    More enhancements to the Future interface

--- src/future.rs
+++ src/future.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::sync::{Arc, Mutex};
 use std::marker::PhantomData;
 
@@ -6,6 +7,27 @@ use rotor::{Notifier, GenericScope};
 trait ReadFuture<T> {
     fn ready(&self) -> bool;
     fn take(&mut self) -> Option<T>;
+    /// Only for debug trait
+    fn peek(&self) -> Option<&T>;
+}
+
+pub trait MakeFuture<T> {
+    fn make_future(self) -> Future<T>;
+}
+
+pub trait GetNotifier {
+    fn get_notifier(self) -> Notifier;
+}
+
+impl<'a, T: GenericScope> GetNotifier for &'a mut T {
+    fn get_notifier(self) -> Notifier {
+        self.notifier()
+    }
+}
+impl GetNotifier for Notifier {
+    fn get_notifier(self) -> Notifier {
+        return self;
+    }
 }
 
 pub struct Future<T>(Arc<Mutex<ReadFuture<T>>>);
@@ -15,6 +37,14 @@ pub struct FutureImpl<I, O, F: FnOnce(I) -> O>{
     convert: Option<F>,
     notifier: Notifier,
     phantom: PhantomData<*const I>,
+}
+
+impl<I, O, F> MakeFuture<O> for Arc<Mutex<FutureImpl<I, O, F>>>
+    where I: 'static, O: 'static, F: FnOnce(I) -> O + 'static
+{
+    fn make_future(self) -> Future<O> {
+        Future(self)
+    }
 }
 
 impl<I, O, F:FnOnce(I) -> O> FutureImpl<I, O, F> {
@@ -42,6 +72,9 @@ impl<I, O, F:FnOnce(I) -> O> ReadFuture<O> for FutureImpl<I, O, F> {
     fn take(&mut self) -> Option<O> {
         self.output.take()
     }
+    fn peek(&self) -> Option<&O> {
+        self.output.as_ref()
+    }
 }
 
 impl<T: Sized> Future<T> {
@@ -57,10 +90,23 @@ impl<T: Sized> Future<T> {
     }
 }
 
-pub fn new<I, O: 'static, F:FnOnce(I) -> O + 'static, S: GenericScope>(scope: &mut S, fun: F)
+pub fn new<I, O, F, N>(notifier: N, fun: F)
     -> Arc<Mutex<FutureImpl<I, O, F>>>
+    where O: 'static, F: FnOnce(I) -> O + 'static, N: GetNotifier
 {
-    Arc::new(Mutex::new(FutureImpl::new(fun, scope.notifier())))
+    Arc::new(Mutex::new(FutureImpl::new(fun, notifier.get_notifier())))
+}
+
+impl<T: fmt::Debug> fmt::Debug for Future<T> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self.0.lock() {
+            Ok(locked) => match locked.peek() {
+                Some(value) => write!(fmt, "Future({:?})", value),
+                None => write!(fmt, "Future(<Waiting>)"),
+            },
+            Err(_) => write!(fmt, "Future(<Poisoned>)"),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -68,8 +114,7 @@ mod test {
     extern crate rotor_test;
     use std::sync::{Arc, Mutex};
 
-    use std::str::FromStr;
-    use super::{new, FutureImpl, Future};
+    use super::{new, FutureImpl, Future, MakeFuture};
 
     trait ParseStr {
         fn put_str(&mut self, &str);
@@ -94,10 +139,10 @@ mod test {
         let mut lp = rotor_test::MockLoop::new(());
         let ref mut scope = lp.scope(1);
         let arc = new(scope, |x: &str| x.parse().unwrap());
-        let future: Future<u64> = Future(arc.clone());
+        let future: Future<u64> = arc.clone().make_future();
         let value: String = String::from("10");
         Port(arc).put(&value[..]);
-        assert_eq!(future.consume().unwrap_or(9999), 10);
+        assert_eq!(future.consume().unwrap(), 10);
     }
 
 }
